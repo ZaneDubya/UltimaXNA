faed700d6d8bbc2e531709baf8f97aef11fb3a3f
zanewagner <zanewagner@e70d1874-5567-11de-b906-09d018dec989>
2009-06-18 05:44:46 +0000

    Added LoginCharList.cs to the SVN ... no actual use for this file yet, but it is listed in the .proj file.

---
+++ trunk/UltimaXNA/UltimaXNA/Code/GUI/Windows/LoginCharList.cs
@@ -0,0 +1,69 @@
+﻿#region File Description & Usings
+//-----------------------------------------------------------------------------
+// GUIWindow_Chat.cs
+//
+// Created by Poplicola
+//-----------------------------------------------------------------------------
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using xWinFormsLib;
+#endregion
+
+namespace UltimaXNA.GUI
+{
+    class Window_CharacterList : Window
+    {
+        public Window_CharacterList(FormCollection nFormCollection)
+            : base(nFormCollection)
+        {
+            //Create a new form
+            m_FormCollection.Add(new Form("frmLoginCharList", "Character List", new Vector2(270, 180), new Vector2(250, 200), Form.BorderStyle.Fixed));
+            m_MyForm = m_FormCollection["frmLoginCharList"];
+
+            m_MyForm.Controls.Add(new Label("label1", new Vector2(10, 30), "Username:", Color.TransparentBlack, Color.Black, 80, Label.Align.Right));
+            m_MyForm.Controls.Add(new Textbox("txtUsername", new Vector2(100, 30), 140, ""));
+            m_MyForm["txtUsername"].Focus();
+
+            m_MyForm.Controls.Add(new Label("label2", new Vector2(10, 60), "Password:", Color.TransparentBlack, Color.Black, 80, Label.Align.Right));
+            m_MyForm.Controls.Add(new Textbox("txtPassword", new Vector2(100, 60), 140, ""));
+
+            m_MyForm.Controls.Add(new Label("label3", new Vector2(10, 90), "Server:", Color.TransparentBlack, Color.Black, 80, Label.Align.Right));
+            m_MyForm.Controls.Add(new Textbox("txtServer", new Vector2(100, 90), 140, "localhost"));
+
+            m_MyForm.Controls.Add(new Label("label4", new Vector2(10, 120), "Port:", Color.TransparentBlack, Color.Black, 80, Label.Align.Right));
+            m_MyForm.Controls.Add(new Textbox("txtPort", new Vector2(100, 120), 140, "2593"));
+
+            m_MyForm.Controls.Add(new Button("btnLogin", new Vector2(100, 150), 140, "Log in", Color.White, Color.Black));
+            m_MyForm["btnLogin"].OnPress = Button1_OnPress;
+            m_MyForm["btnLogin"].OnRelease = Button1_OnRelease;
+
+            //Show the form
+            this.Show();
+            m_MyForm.CloseButton.OnRelease += Form_OnClose; // Respond when we close the login window ...
+        }
+
+        private void Form_OnClose(object obj, EventArgs e)
+        {
+            // We closed the window before logging in.
+            Events.QuitImmediate();
+        }
+
+        public void CharListPacket(Network.Packet nPacket)
+        {
+
+        }
+
+        private void Button1_OnPress(object obj, EventArgs e)
+        {
+            //((Listbox)formCollection["form1"]["listbox1"]).Add(formCollection["form1"].Controls["textbox1"].Text);
+        }
+        private void Button1_OnRelease(object obj, EventArgs e)
+        {
+            // make sure we don't log in twice
+            m_MyForm["btnLogin"].Enabled = false;
+            // send the login event
+            Events.Login(m_MyForm["txtServer"].Text, System.Convert.ToInt32(m_MyForm["txtPort"].Text), m_MyForm["txtUsername"].Text, m_MyForm["txtPassword"].Text);
+        }
+    }
+}
