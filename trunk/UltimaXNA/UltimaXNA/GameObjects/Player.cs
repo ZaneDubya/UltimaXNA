c80b3cd5fd0951bc69c895d9f6a257c85eb94661
zanewagner@gmail.com <zanewagner@gmail.com@e70d1874-5567-11de-b906-09d018dec989>
2009-07-14 23:46:06 +0000

    Two fixes for animations: Better support for the speed of all animations except for movement, and the client's player object does not 'hold' animations anymore.

--- trunk/UltimaXNA/UltimaXNA/GameObjects/Player.cs
+++ trunk/UltimaXNA/UltimaXNA/GameObjects/Player.cs
@@ -11,26 +11,11 @@ namespace UltimaXNA.GameObjects
 {
     public class Player : UltimaXNA.GameObjects.Unit
     {
-        public int FlagsPlayer = 0;
-        public int GuildID = 0, GuildRank = 0;
-        public int ChosenTitle = 0;
-        public int BytesPlayer0 = 0, BytesPlayer1 = 0, BytesPlayer2 = 0, BytesPlayer3 = 0;
-        public int Experience = 0, ExperienceNextLevel = 0, PetExperience = 0, PetNextLevelExperience = 0;
-        public int Coinage = 0;
-
-        public int PartyID = 0;
-        public int DuelOpponentSerial = 0;
-
-        public int[] EquipSlots = new int[12];
-        public int[] PackSlots = new int[20];
-        public int[] BankSlots = new int[30];
-
-        public PlayerQuest[] QuestLog = new PlayerQuest[20];
-
         public Player(Serial serial)
             : base(serial)
         {
             this.ObjectType = ObjectType.Player;
+            m_Animation.HoldAnimationMS = 0;
         }
 
         public override string ToString()
