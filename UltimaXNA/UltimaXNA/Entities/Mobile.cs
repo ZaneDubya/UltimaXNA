84237cd4d08c5af0182aea59ab41646bfb955277
Zane Wagner <ZaneDubya@users.noreply.github.com>
2009-07-16 18:28:04 +0000

    Fix for compile errors ?

--- UltimaXNA/UltimaXNA/Entities/Mobile.cs
+++ UltimaXNA/UltimaXNA/Entities/Mobile.cs
@@ -11,46 +11,36 @@ using Microsoft.Xna.Framework;
 
 namespace UltimaXNA.GameObjects
 {
-    public class Unit : UltimaXNA.GameObjects.BaseObject
+    public class Mobile : Entity
     {
-		// Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-		private int m_DisplayBodyID = 0;
+        public string Name = string.Empty;
+        public CurrentMaxValue Health, Stamina, Mana;
+        internal WornEquipment equipment;
+        internal MobileAnimation animation;
+
+        public bool IsMounted { get { return equipment[(int)EquipLayer.Mount] != null; } }
+        public bool IsWarMode { get { return animation.WarMode; } set { animation.WarMode = value; } }
+        
 		public int DisplayBodyID
 		{
-			get { return m_DisplayBodyID; }
-			set
-			{
-				m_DisplayBodyID = value;
-				if ( m_Animation != null )
-					m_Animation.BodyID = m_DisplayBodyID;
-			}
+			get { return animation.BodyID; }
+            set { animation.BodyID = value; }
 		}
-		// Issue 6 - End
 
-        public WornEquipment Equipment;
-
-        private int m_Hue;
+        private int _hue;
         public int Hue // Fix for large hue values per issue12 (http://code.google.com/p/ultimaxna/issues/detail?id=12) --ZDW 6/15/2009
         {
-            get { return m_Hue; }
+            get { return _hue; }
             set
             {
                 if (value > 2998)
-                    m_Hue = (int)(value / 32);
+                    _hue = (int)(value / 32);
                 else
-                    m_Hue = value;
+                    _hue = value;
             }
         }
 
-        public string Name = string.Empty;
-        public CurrentMaxValue Health, Stamina, Mana;
-
-        public bool IsMounted
-        {
-            get { return Equipment[(int)EquipLayer.Mount] != null; }
-        }
 		// Issue 14 - Wrong layer draw order - http://code.google.com/p/ultimaxna/issues/detail?id=14 - Smjert
-		// Changed the order for Mount and Helm
 		private int[] m_DrawLayers = new int[20]
 		{
 			(int)EquipLayer.Mount,
@@ -74,64 +64,45 @@ namespace UltimaXNA.GameObjects
 			(int)EquipLayer.Helm,
             (int)EquipLayer.TwoHanded,
 		};
-		// Issue 14 - End
 
-        public UnitAnimation m_Animation;
-
-        public Unit(Serial serial)
+        public Mobile(Serial serial)
             : base(serial)
         {
-            ObjectType = ObjectType.Unit;
-
-            Equipment = new WornEquipment(this);
-
+            animation = new MobileAnimation();
+            equipment = new WornEquipment(this);
             Movement.RequiresUpdate = true;
-            m_Animation = new UnitAnimation();
-			// Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-			m_Animation.BodyID = DisplayBodyID;
-			// Issue 6 - End
         }
 
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
             base.Update(gameTime);
-
-            m_Animation.Update(gameTime);
-
-            // Check to see if our health, stamina, or mana has been updated. If so, announce it to the GUI.
-            if (Health.Updated || Stamina.Updated || Mana.Updated)
-            {
-                Health.Updated = false;
-                Stamina.Updated = false;
-                Mana.Updated = false;
-            }
+            animation.Update(gameTime);
         }
 
-        protected override void Draw(UltimaXNA.TileEngine.MapCell nCell, Vector3 nLocation, Vector3 nOffset)
+        internal override void Draw(UltimaXNA.TileEngine.MapCell nCell, Vector3 nLocation, Vector3 nOffset)
         {
             if (Movement.IsMoving)
             {
-                bool nRunning = false;
-                UnitActions iAnimationAction = (nRunning == true) ? UnitActions.run : UnitActions.walk;
-                m_Animation.SetAnimation(iAnimationAction, 10, 0, false, false, 1);
+                MobileAction iAnimationAction = (Movement.IsRunning) ? MobileAction.Run : MobileAction.Walk;
+                animation.SetAnimation(iAnimationAction, 10, 0, false, false, 0);
             }
             else
             {
-                m_Animation.SetAnimation(UnitActions.stopmovement);
+                if (animation.IsMovementAction(animation.Action))
+                    animation.HaltAnimation = true;
             }
 
             int iDirection = Movement.DrawFacing;
 
 			// Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-			m_Animation.Mounted = false;
-            GameObject mount = Equipment[(int)EquipLayer.Mount];
+            Item mount = equipment[(int)EquipLayer.Mount];
 			TileEngine.MobileTile mobtile = null;
             if (mount != null && mount.ObjectTypeID != 0)
             {
-                Movement.Mounted = m_Animation.Mounted = true;
+                Movement.Mounted = animation.IsMounted = true;
                 mobtile = new TileEngine.MobileTile(
                                 mount.ObjectTypeID, nLocation, nOffset,
-                                iDirection, m_Animation.Action == UnitActions.nothing ? 2 : (int)m_Animation.Action, m_Animation.AnimationFrame,
+                                iDirection, (int)animation.Action, animation.AnimationFrame,
                                 Serial, 0x1A, mount.Hue, false);
 
                 mobtile.SubType = TileEngine.MobileTileTypes.Mount;
@@ -139,12 +110,12 @@ namespace UltimaXNA.GameObjects
             }
             else
             {
-                Movement.Mounted = m_Animation.Mounted = false;
+                Movement.Mounted = animation.IsMounted = false;
             }
 
-			int iAction = m_Animation.GetAction_People();
+			int action = animation.GetAction();
             
-			mobtile = new TileEngine.MobileTile(DisplayBodyID, nLocation, nOffset, iDirection, iAction, m_Animation.AnimationFrame, Serial, 1, Hue, m_Animation.Mounted);
+			mobtile = new TileEngine.MobileTile(DisplayBodyID, nLocation, nOffset, iDirection, action, animation.AnimationFrame, Serial, 1, Hue, animation.IsMounted);
 			mobtile.SubType = TileEngine.MobileTileTypes.Body;
 			nCell.AddMobileTile(mobtile);
 			// Issue 6 - End
@@ -153,16 +124,15 @@ namespace UltimaXNA.GameObjects
             for (int i = 0; i < m_DrawLayers.Length; i++)
             {
                 // Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-                if ( Equipment[m_DrawLayers[i]] != null && Equipment[m_DrawLayers[i]].AnimationDisplayID != 0 )
+                if ( equipment[m_DrawLayers[i]] != null && equipment[m_DrawLayers[i]].AnimationDisplayID != 0 )
                 {
                     mobtile = new TileEngine.MobileTile(
-                            Equipment[m_DrawLayers[i]].AnimationDisplayID, nLocation, nOffset,
-                            iDirection, iAction, m_Animation.AnimationFrame,
-                            Serial, i + 1, Equipment[m_DrawLayers[i]].Hue, m_Animation.Mounted);
+                            equipment[m_DrawLayers[i]].AnimationDisplayID, nLocation, nOffset,
+                            iDirection, action, animation.AnimationFrame,
+                            Serial, i + 1, equipment[m_DrawLayers[i]].Hue, animation.IsMounted);
 					
                     mobtile.SubType = TileEngine.MobileTileTypes.Equipment;
                     nCell.AddMobileTile(mobtile);
-					
                 }
                 // Issue 6 - End
             }
@@ -170,374 +140,25 @@ namespace UltimaXNA.GameObjects
 
         public override void Dispose()
         {
-            // clear the objects that this mobile is wearing.
-            Equipment.ClearEquipment();
+            equipment.ClearEquipment();
             base.Dispose();
         }
 
         public void UnWearItem(Serial serial)
         {
-            //unwear this item!
-            Equipment.RemoveBySerial(serial);
+            equipment.RemoveBySerial(serial);
         }
 
         public void Animation(int action, int frameCount, int repeatCount, bool reverse, bool repeat, int delay)
         {
-            m_Animation.SetAnimation((UnitActions)action, frameCount, repeatCount, reverse, repeat, delay);
+            animation.SetAnimation((MobileAction)action, frameCount, repeatCount, reverse, repeat, delay);
         }
 
-        public void SetFacing(int nDirection)
+        public void Move(int nX, int nY, int nZ, int nFacing)
         {
-            // This should be called exclusively from the server...
-            Movement.Facing = (Direction)(nDirection & 0x0F);
-        }
-
-        public void Move(int nX, int nY, int nZ)
-        {
+            if (nFacing != -1)
+                Movement.Facing = (Direction)nFacing;
             Movement.SetGoalTile((float)nX, (float)nY, (float)nZ);
         }
     }
-
-    public class UnitAnimation
-    {
-		// Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-        public UnitActions Action;
-		// Issue 6 - End
-        public float AnimationFrame = 0f;
-        private float m_AnimationStep = 0f;
-		// Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-		public bool Mounted = false;
-		public int BodyID;
-		// Issue 6 - End
-
-        public UnitAnimation()
-        {
-            m_AnimationStep = 0f;
-            Action = UnitActions.nothing;
-        }
-
-        public void SetAnimation(UnitActions nAction)
-        {
-            if (nAction == UnitActions.stopmovement)
-            {
-                if ((Action == UnitActions.walk) ||
-                    (Action == UnitActions.walkfaster) ||
-                    (Action == UnitActions.run) ||
-                    (Action == UnitActions.runfaster))
-                {
-                    this.SetAnimation(UnitActions.nothing);
-                }
-                return;
-            }
-
-            if (Action != nAction)
-            {
-                Action = (UnitActions)nAction;
-                AnimationFrame = 0f;
-                m_AnimationStep = 0f;
-            }
-        }
-
-        private int _FrameCount;
-        private int _FrameDelay;
-        public void SetAnimation(UnitActions action, int frameCount, int repeatCount, bool reverse, bool repeat, int delay)
-        {
-            if (Action != action)
-            {
-                Action = (UnitActions)action;
-                AnimationFrame = 0f;
-                _FrameCount = frameCount;
-                _FrameDelay = delay;
-            }
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            m_AnimationStep = (float)((_FrameCount * (_FrameDelay + 1)) * 5);
-            if (m_AnimationStep != 0)
-            {
-                AnimationFrame += 1f / m_AnimationStep / ((1f / 60f) / (float)(gameTime.ElapsedRealTime.TotalMilliseconds / 1000f));
-                if (AnimationFrame >= 1f)
-                {
-                    AnimationFrame %= 1f;
-                }
-            }
-            else
-                AnimationFrame = 0;
-        }
-
-        public int GetAction_People()
-        {
-			// Issue 6 - Missing mounted animations - http://code.google.com/p/ultimaxna/issues/detail?id=6 - Smjert
-			if(Mounted)
-			{
-				switch (Action)
-				{
-					case UnitActions.walk:
-						return (int)23;
-					case UnitActions.run:
-						return (int)24;
-					case UnitActions.nothing:
-						return (int)25;
-
-					default: return (int)Action;
-				}
-			}
-
-			// Hiryu has different stand animation
-			if(BodyID == 243 && Action == UnitActions.nothing)
-				return 2;
-
-			return (int)Action;
-			// Issue 6 - End
-        }
-    }
-
-    #region UnitEnums
-    public enum UnitActions : int
-    {
-        stopmovement = 0x7fffffff,
-        walk = 0x00,
-        walkfaster = 0x01,
-        run = 0x02,
-        runfaster = 0x03,
-        nothing = 0x04,
-        shiftshoulders = 0x05,
-        handsonhips = 0x06,
-        attackstanceshort = 0x07,
-        attackstancelonger = 0x08,
-        swingattackwithknofe = 0x09,
-        stabunderhanded = 0x0a,
-        swingattackoverhandwithsword = 0x0b,
-        swingattackwithswordoverandside = 0x0c,
-        swingattackwithswordside = 0x0d,
-        stabwithpointofsword = 0x0e,
-        readystance = 0x0f,
-        magicbutterchurn = 0x10,
-        handsoverheadbalerina = 0x11,
-        bowshot = 0x12,
-        crossbow = 0x13,
-        gethit = 0x14,
-        falldownanddiebackwards = 0x15,
-        falldownanddieforwards = 0x16,
-        ridehorselong = 0x17,
-        ridehorsemedium = 0x18,
-        ridehorseshort = 0x19,
-        swingswordfromhorse = 0x1a,
-        normalbowshotonhorse = 0x1b,
-        crossbowshot = 0x1c,
-        block2onhorsewithshield = 0x1d,
-        blockongroundwithshield = 0x1e,
-        swinginterrupt = 0x1f,
-        bowdeep = 0x20,
-        salute = 0x21,
-        scratchhead = 0x22,
-        onefootforwardfor2secs = 0x23,
-        same = 0x24
-    }
-    public enum EquipLayer : int
-    {
-        /// <summary>
-        /// Invalid layer.
-        /// </summary>
-        Invalid = 0x00,
-        /// <summary>
-        /// First valid layer. Equivalent to <c>Layer.OneHanded</c>.
-        /// </summary>
-        FirstValid = 0x01,
-        /// <summary>
-        /// One handed weapon.
-        /// </summary>
-        OneHanded = 0x01,
-        /// <summary>
-        /// Two handed weapon or shield.
-        /// </summary>
-        TwoHanded = 0x02,
-        /// <summary>
-        /// Shoes.
-        /// </summary>
-        Shoes = 0x03,
-        /// <summary>
-        /// Pants.
-        /// </summary>
-        Pants = 0x04,
-        /// <summary>
-        /// Shirts.
-        /// </summary>
-        Shirt = 0x05,
-        /// <summary>
-        /// Helmets, hats, and masks.
-        /// </summary>
-        Helm = 0x06,
-        /// <summary>
-        /// Gloves.
-        /// </summary>
-        Gloves = 0x07,
-        /// <summary>
-        /// Rings.
-        /// </summary>
-        Ring = 0x08,
-        /// <summary>
-        /// Talismans.
-        /// </summary>
-        Talisman = 0x09,
-        /// <summary>
-        /// Gorgets and necklaces.
-        /// </summary>
-        Neck = 0x0A,
-        /// <summary>
-        /// Hair.
-        /// </summary>
-        Hair = 0x0B,
-        /// <summary>
-        /// Half aprons.
-        /// </summary>
-        Waist = 0x0C,
-        /// <summary>
-        /// Torso, inner layer.
-        /// </summary>
-        InnerTorso = 0x0D,
-        /// <summary>
-        /// Bracelets.
-        /// </summary>
-        Bracelet = 0x0E,
-        /// <summary>
-        /// Unused.
-        /// </summary>
-        Unused_xF = 0x0F,
-        /// <summary>
-        /// Beards and mustaches.
-        /// </summary>
-        FacialHair = 0x10,
-        /// <summary>
-        /// Torso, outer layer.
-        /// </summary>
-        MiddleTorso = 0x11,
-        /// <summary>
-        /// Earings.
-        /// </summary>
-        Earrings = 0x12,
-        /// <summary>
-        /// Arms and sleeves.
-        /// </summary>
-        Arms = 0x13,
-        /// <summary>
-        /// Cloaks.
-        /// </summary>
-        Cloak = 0x14,
-        /// <summary>
-        /// Backpacks.
-        /// </summary>
-        Backpack = 0x15,
-        /// <summary>
-        /// Torso, outer layer.
-        /// </summary>
-        OuterTorso = 0x16,
-        /// <summary>
-        /// Leggings, outer layer.
-        /// </summary>
-        OuterLegs = 0x17,
-        /// <summary>
-        /// Leggings, inner layer.
-        /// </summary>
-        InnerLegs = 0x18,
-        /// <summary>
-        /// Last valid non-internal layer. Equivalent to <c>Layer.InnerLegs</c>.
-        /// </summary>
-        LastUserValid = 0x18,
-        /// <summary>
-        /// Mount item layer.
-        /// </summary>
-        Mount = 0x19,
-        /// <summary>
-        /// Vendor 'buy pack' layer.
-        /// </summary>
-        ShopBuy = 0x1A,
-        /// <summary>
-        /// Vendor 'resale pack' layer.
-        /// </summary>
-        ShopResale = 0x1B,
-        /// <summary>
-        /// Vendor 'sell pack' layer.
-        /// </summary>
-        ShopSell = 0x1C,
-        /// <summary>
-        /// Bank box layer.
-        /// </summary>
-        Bank = 0x1D,
-        /// <summary>
-        /// Last valid layer. Equivalent to <c>Layer.Bank</c>.
-        /// </summary>
-        LastValid = 0x1D
-    }
-    #endregion
-    #region WornEquipmentClass
-    public class WornEquipment
-    {
-        private GameObject[] m_Equipment;
-        private Unit m_Owner;
-        private int m_UpdateTicker = 0;
-
-        public int UpdateTicker
-        {
-            get { return m_UpdateTicker; }
-        }
-
-        public WornEquipment(Unit nOwner)
-        {
-            m_Equipment = new GameObject[(int)EquipLayer.LastValid + 1];
-            m_Owner = nOwner;
-        }
-
-        public GameObject this[int nIndex]
-        {
-            get
-            {
-                if (nIndex > (int)EquipLayer.LastValid)
-                    return null;
-                return m_Equipment[nIndex];
-            }
-            set
-            {
-                if (value == null)
-                {
-                    if (m_Equipment[nIndex] != null)
-                    {
-                        m_Equipment[nIndex].Wearer = null;
-                        m_Equipment[nIndex].Dispose();
-                        m_Equipment[nIndex] = null;
-                    }
-                }
-                else
-                {
-                    m_Equipment[nIndex] = value;
-                    value.Wearer = m_Owner;
-                }
-                m_UpdateTicker++;
-            }
-        }
-
-        public void ClearEquipment()
-        {
-            for (int i = 0; i <= (int)EquipLayer.LastValid; i++)
-            {
-                this[i] = null;
-            }
-            m_UpdateTicker++;
-        }
-
-        public void RemoveBySerial(Serial serial)
-        {
-            for (int i = 0; i <= (int)EquipLayer.LastValid; i++)
-            {
-                if (this[i] != null)
-                    if (this[i].Serial == serial)
-                    {
-                        this[i] = null;
-                    }
-            }
-            m_UpdateTicker++;
-        }
-    }
-    #endregion
 }
