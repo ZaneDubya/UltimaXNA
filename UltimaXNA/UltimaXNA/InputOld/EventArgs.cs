642995047249276735f0420a328728f16a575804
Zane Wagner <ZaneDubya@users.noreply.github.com>
2010-10-17 20:12:44 +0000

    * Update that adds InputOld files - thanks KKLOLK!

---
+++ UltimaXNA/UltimaXNA/InputOld/EventArgs.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace UltimaXNA.InputOld
+{
+    public class MouseEventArgs : EventArgs
+    {
+        bool _handled = false;
+
+        readonly Vector2 _movementDelta;
+        readonly Vector2 _position;
+
+        readonly MouseButton[] _buttonsDown;
+        readonly MouseButton[] _buttonsUp;
+        readonly MouseButton[] _buttonsPressed;
+        readonly MouseButton[] _buttonsReleased;
+
+        /// <summary>
+        /// Gets an array of buttons that were down during the frame the the event was called
+        /// </summary>
+        public MouseButton[] ButtonsDown
+        {
+            get { return _buttonsDown; }
+        }
+
+        /// <summary>
+        /// Gets an array of buttons that were up during the frame the the event was called
+        /// </summary>
+        public MouseButton[] ButtonsUp
+        {
+            get { return _buttonsUp; }
+        }
+
+        /// <summary>
+        /// Gets an array of buttons that were pressed during the frame the the event was called
+        /// </summary>
+        public MouseButton[] ButtonsPressed
+        {
+            get { return _buttonsPressed; }
+        }
+
+        /// <summary>
+        /// Gets an array of buttons that were released during the frame the the event was called
+        /// </summary>
+        public MouseButton[] ButtonsReleased
+        {
+            get { return _buttonsReleased; }
+        }
+
+        /// <summary>
+        /// Gets the current position mouse in screen coordinates
+        /// </summary>
+        public Vector2 Position
+        {
+            get { return _position; }
+        }
+
+        /// <summary>
+        /// Gets the current X location of the mouse in screen coordinates
+        /// </summary>
+        public int X
+        {
+            get { return (int)_position.X; }
+        }
+        
+        /// <summary>
+        /// Gets the current Y location of the mouse in screen coordinates
+        /// </summary>
+        public int Y
+        {
+            get { return (int)_position.Y; }
+        } 
+        
+        /// <summary>
+        /// Gets a boolean value indicating that the event was or was not handled.
+        /// </summary>
+        public bool Handled
+        {
+            get { return _handled; }
+            set { _handled = value; }
+        }
+
+        /// <summary>
+        /// Gets the movement delta of the mouse from the previous to the current frame
+        /// </summary>
+        public Vector2 MovementDelta
+        {
+            get { return _movementDelta; }
+        } 
+
+        /// <summary>
+        /// Creates a new instance of MouseEventArgs
+        /// </summary>
+        public MouseEventArgs(MouseButton[] buttonsDown, 
+            MouseButton[] buttonsUp, MouseButton[] buttonsPressed, MouseButton[] buttonsReleased, Vector2 position, Vector2 movementDelta)
+        {
+            _position = position;
+            _buttonsDown = buttonsDown;
+            _buttonsUp = buttonsUp;
+            _buttonsPressed = buttonsPressed;
+            _buttonsReleased = buttonsReleased;
+            _movementDelta = movementDelta;
+        }
+
+        /// <summary>
+        /// Returns a Boolean value indicating if the button was up during the event
+        /// </summary>
+        public bool ContainsButtonUp(MouseButton button)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _buttonsUp.Length && !found; i++)
+            {
+                found = _buttonsUp[i] == button;
+            }
+
+            return found;
+        }
+
+        /// <summary>
+        /// Returns a Boolean value indicating if the button was down during the event
+        /// </summary>
+        public bool ContainsButtonDown(MouseButton button)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _buttonsDown.Length && !found; i++)
+            {
+                found = _buttonsDown[i] == button;
+            }
+
+            return found;
+        }
+
+        /// <summary>
+        /// Returns a Boolean value indicating if the button was pressed during the event
+        /// </summary>
+        public bool ContainsButtonPressed(MouseButton button)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _buttonsPressed.Length && !found; i++)
+            {
+                found = _buttonsPressed[i] == button;
+            }
+
+            return found;
+        }
+
+        /// <summary>
+        /// Returns a Boolean value indicating if the button was released during the event
+        /// </summary>
+        public bool ContainsButtonReleased(MouseButton button)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _buttonsReleased.Length && !found; i++)
+            {
+                found = _buttonsReleased[i] == button;
+            }
+
+            return found;
+        }
+    }
+
+    public class KeyboardEventArgs : EventArgs
+    {
+        readonly Keys[] _keysUp;
+        readonly Keys[] _keysDown;
+        readonly Keys[] _keysPressed;
+        readonly Keys[] _keysReleased;
+
+        bool _handled = false;
+
+        public Keys[] KeysUp
+        {
+            get { return _keysUp; }
+        }
+
+        public Keys[] KeysDown
+        {
+            get { return _keysDown; }
+        }
+
+        public Keys[] KeysPressed
+        {
+            get { return _keysPressed; }
+        } 
+
+        public Keys[] KeysReleased
+        {
+            get { return _keysReleased; }
+        } 
+
+        public bool Handled
+        {
+            get { return _handled; }
+            set { _handled = value; }
+        }
+
+        public KeyboardEventArgs(Keys[] keysUp, Keys[] keysDown, Keys[] keysPressed, Keys[] keysReleased)
+        {
+            _keysUp = keysUp;
+            _keysDown = keysDown;
+            _keysPressed = keysPressed;
+            _keysReleased = keysReleased;
+        }
+
+        public bool ContainsKeyUp(Keys key)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _keysUp.Length && !found; i++)
+            {
+                found = _keysUp[i] == key;
+            }
+
+            return found;
+        }
+
+        public bool ContainsKeyDown(Keys key)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _keysDown.Length && !found; i++)
+            {
+                found = _keysDown[i] == key;
+            }
+
+            return found;
+        }
+
+        public bool ContainsKeyPressed(Keys key)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _keysPressed.Length && !found; i++)
+            {
+                found = _keysPressed[i] == key;
+            }
+
+            return found;
+        }
+
+        public bool ContainsKeyReleased(Keys key)
+        {
+            bool found = false;
+
+            for (int i = 0; i < _keysReleased.Length && !found; i++)
+            {
+                found = _keysReleased[i] == key;
+            }
+
+            return found;
+        }
+    }
+}
---
+++ UltimaXNA/UltimaXNA/InputOld/IInputService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace UltimaXNA.InputOld
+{
+    public interface IInputService : IGameComponent
+    {
+        Vector2 MousePosition { get; }
+
+        event EventHandler<KeyboardEventArgs> KeyDown;
+        event EventHandler<KeyboardEventArgs> KeyUp;
+        event EventHandler<MouseEventArgs> MouseDown;
+        event EventHandler<MouseEventArgs> MouseUp;
+        event EventHandler<MouseEventArgs> MouseMove;
+
+        bool IsKeyDown(Keys key);
+        bool IsKeyPress(Keys key);
+        bool IsKeyRelease(Keys key);
+        bool IsKeyUp(Keys key);
+
+        bool IsMouseButtonDown(MouseButton mb);
+        bool IsMouseButtonPress(MouseButton mb);
+        bool IsMouseButtonRelease(MouseButton mb);
+        bool IsMouseButtonUp(MouseButton mb);
+        bool IsSpecialKey(Keys key);
+        bool IsCursorMovedSinceLastUpdate();
+
+        float TimePressed(MouseButton mb);
+        float TimePressed(Keys key);
+
+        void GetKeyboardInput(out string inputText, out List<Keys> specialKeys);
+        char KeyToChar(Keys key, bool shiftPressed);
+    }
+}
