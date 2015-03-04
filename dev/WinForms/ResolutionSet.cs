42002224add90902ab5d969a2673e8ee7d206cfd
Eskymák <git@eskymak.eu>
2015-03-04 19:41:25 +0200

    Screen resolution dialog
    Added screen resolution dialog to be able to change client resolution

---
+++ dev/WinForms/ResolutionSet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UltimaXNA.WinForms
+{
+    public partial class ResolutionSet : Form
+    {
+        public ResolutionSet()
+        {
+            InitializeComponent();
+        }
+
+        public int getWidth()
+        {
+            int width = Int32.Parse(widthBox.Text);
+            return width;
+        }
+        public int getHeight()
+        {
+            int height = Int32.Parse(heightBox.Text);
+            return height;
+        }
+    }
+}
---
+++ dev/WinForms/ResolutionSet.designer.cs
@@ -0,0 +1,97 @@
+﻿namespace UltimaXNA.WinForms
+{
+    partial class ResolutionSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.widthBox = new System.Windows.Forms.TextBox();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // widthBox
+            // 
+            this.widthBox.Location = new System.Drawing.Point(12, 12);
+            this.widthBox.Name = "widthBox";
+            this.widthBox.Size = new System.Drawing.Size(100, 20);
+            this.widthBox.TabIndex = 0;
+            this.widthBox.Text = "800";
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(12, 38);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(100, 20);
+            this.heightBox.TabIndex = 1;
+            this.heightBox.Text = "600";
+            // 
+            // StartButton
+            // 
+            this.StartButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.StartButton.Location = new System.Drawing.Point(13, 92);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(99, 23);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(13, 122);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(99, 23);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // ResolutionSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(124, 162);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.widthBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ResolutionSet";
+            this.Text = "ResolutionSet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox widthBox;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
