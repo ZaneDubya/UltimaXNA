af0812445fb7043a0ced33969bf5322bde4d9d45
Vodacek <vodacek@zvb.cz>
2015-03-18 11:04:01 +0100

    Paperdoll
    other mobiles can now have paperdolls

--- dev/UltimaGUI/WorldGumps/PaperDollGump.cs
+++ dev/UltimaGUI/WorldGumps/PaperDollGump.cs
@@ -12,6 +12,7 @@ using Microsoft.Xna.Framework;
 using UltimaXNA.Entity;
 using UltimaXNA.Rendering;
 using UltimaXNA.UltimaGUI.Controls;
+using UltimaXNA.UltimaWorld;
 
 namespace UltimaXNA.UltimaGUI.WorldGumps
 {
@@ -37,38 +38,57 @@ namespace UltimaXNA.UltimaGUI.WorldGumps
             m_Parent = parent;
 
             IsMovable = true;
-            AddControl(new GumpPic(this, 0, 0, 0, 0x07d0, 0));
-            LastControl.MakeDragger(this);
-            LastControl.MakeCloseTarget(this);
 
-            // HELP
-            AddControl(new Button(this, 0, 185, 44 + 27 * 0, 0x07ef, 0x07f0, ButtonTypes.Activate, 0, (int)Buttons.Help));
-            ((Button)LastControl).GumpOverID = 0x07f1;
-            // OPTIONS
-            AddControl(new Button(this, 0, 185, 44 + 27 * 1, 0x07d6, 0x07d7, ButtonTypes.Activate, 0, (int)Buttons.Options));
-            ((Button)LastControl).GumpOverID = 0x07d8;
-            // LOG OUT
-            AddControl(new Button(this, 0, 185, 44 + 27 * 2, 0x07d9, 0x07da, ButtonTypes.Activate, 0, (int)Buttons.LogOut));
-            ((Button)LastControl).GumpOverID = 0x07db;
-            // QUESTS
-            AddControl(new Button(this, 0, 185, 44 + 27 * 3, 0x57b5, 0x57b7, ButtonTypes.Activate, 0, (int)Buttons.Quests));
-            ((Button)LastControl).GumpOverID = 0x57b6;
-            // SKILLS
-            AddControl(new Button(this, 0, 185, 44 + 27 * 4, 0x07df, 0x07e0, ButtonTypes.Activate, 0, (int)Buttons.Skills));
-            ((Button)LastControl).GumpOverID = 0x07e1;
-            // GUILD
-            AddControl(new Button(this, 0, 185, 44 + 27 * 5, 0x57b2, 0x57b4, ButtonTypes.Activate, 0, (int)Buttons.Guild));
-            ((Button)LastControl).GumpOverID = 0x57b3;
-            // PEACE / WAR
-            AddControl(new Button(this, 0, 185, 44 + 27 * 6, 0x07e5, 0x07e6, ButtonTypes.Activate, 0, (int)Buttons.PeaceWarToggle));
-            ((Button)LastControl).GumpOverID = 0x07e7;
-            // STATUS
-            AddControl(new Button(this, 0, 185, 44 + 27 * 7, 0x07eb, 0x07ec, ButtonTypes.Activate, 0, (int)Buttons.Status));
-            ((Button)LastControl).GumpOverID = 0x07ed;
+            if (parent == (Mobile)EntityManager.GetPlayerObject())
+            {
+                AddControl(new GumpPic(this, 0, 0, 0, 0x07d0, 0));
+
+                // HELP
+                AddControl(new Button(this, 0, 185, 44 + 27*0, 0x07ef, 0x07f0, ButtonTypes.Activate, 0,
+                    (int) Buttons.Help));
+                ((Button) LastControl).GumpOverID = 0x07f1;
+                // OPTIONS
+                AddControl(new Button(this, 0, 185, 44 + 27*1, 0x07d6, 0x07d7, ButtonTypes.Activate, 0,
+                    (int) Buttons.Options));
+                ((Button) LastControl).GumpOverID = 0x07d8;
+                // LOG OUT
+                AddControl(new Button(this, 0, 185, 44 + 27*2, 0x07d9, 0x07da, ButtonTypes.Activate, 0,
+                    (int) Buttons.LogOut));
+                ((Button) LastControl).GumpOverID = 0x07db;
+                // QUESTS
+                AddControl(new Button(this, 0, 185, 44 + 27*3, 0x57b5, 0x57b7, ButtonTypes.Activate, 0,
+                    (int) Buttons.Quests));
+                ((Button) LastControl).GumpOverID = 0x57b6;
+                // SKILLS
+                AddControl(new Button(this, 0, 185, 44 + 27*4, 0x07df, 0x07e0, ButtonTypes.Activate, 0,
+                    (int) Buttons.Skills));
+                ((Button) LastControl).GumpOverID = 0x07e1;
+                // GUILD
+                AddControl(new Button(this, 0, 185, 44 + 27*5, 0x57b2, 0x57b4, ButtonTypes.Activate, 0,
+                    (int) Buttons.Guild));
+                ((Button) LastControl).GumpOverID = 0x57b3;
+                // PEACE / WAR
+                AddControl(new Button(this, 0, 185, 44 + 27*6, 0x07e5, 0x07e6, ButtonTypes.Activate, 0,
+                    (int) Buttons.PeaceWarToggle));
+                ((Button) LastControl).GumpOverID = 0x07e7;
+                // STATUS
+                AddControl(new Button(this, 0, 185, 44 + 27*7, 0x07eb, 0x07ec, ButtonTypes.Activate, 0,
+                    (int) Buttons.Status));
+                ((Button) LastControl).GumpOverID = 0x07ed;
+            }
+            else
+            {
+                AddControl(new GumpPic(this, 0, 0, 0, 0x07d1, 0));
+            }
 
             // Paperdoll
-            AddControl(new PaperDollInteractable(this, 0, 8, 21));
-            ((PaperDollInteractable)LastControl).SourceEntity = m_Parent;
+            AddControl(new PaperDollInteractable(this, 0, 8, 21)
+            {
+                SourceEntity = m_Parent
+            });
+
+            LastControl.MakeDragger(this);
+            LastControl.MakeCloseTarget(this);
         }
 
         public override void Update(GameTime gameTime)
