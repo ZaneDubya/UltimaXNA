0e1cb748266fa671e3f20c76f9db6de7e3cfecf3
robertdeclaux <ftbasol@gmail.com>
2015-03-28 10:46:24 +0200

    previous-next speech
    CTRL-Q gets last speech to input
    CTRL-W gets next speech to input

--- dev/UltimaGUI/WorldGumps/ChatWindow.cs
+++ dev/UltimaGUI/WorldGumps/ChatWindow.cs
@@ -12,6 +12,7 @@ using Microsoft.Xna.Framework;
 using UltimaXNA.Core.Rendering;
 using UltimaXNA.UltimaGUI.Controls;
 using UltimaXNA.UltimaData.FontsNew;
+using InterXLib.Input.Windows;
 
 namespace UltimaXNA.UltimaGUI.WorldGumps
 {
@@ -19,11 +20,14 @@ namespace UltimaXNA.UltimaGUI.WorldGumps
     {
         TextEntry InputState;
         List<ChatLineTimed> m_textEntries;
+        List<string> m_messages;
+        int messageIndex = -1;
 
         public ChatWindow()
             : base(0, 0)
         {
             m_textEntries = new List<ChatLineTimed>();
+            m_messages = new List<string>();
             Width = 400;
         }
 
@@ -48,6 +52,24 @@ namespace UltimaXNA.UltimaGUI.WorldGumps
                 }
             }
 
+            if (UltimaEngine.Input.HandleKeyboardEvent(KeyboardEventType.Down, WinKeys.Q, false, false, true) && messageIndex > -1)
+            {
+                if (messageIndex > 0)
+                    messageIndex -= 1;
+                InputState.Text = m_messages[messageIndex];
+
+            }
+            else if (UltimaEngine.Input.HandleKeyboardEvent(KeyboardEventType.Down, WinKeys.W, false, false, true))
+            {
+                if (messageIndex < m_messages.Count - 1)
+                {
+                    messageIndex += 1;
+                    InputState.Text = m_messages[messageIndex];
+                }
+                else
+                    InputState.Text = "";
+            }
+
             base.Update(gameTime);
         }
 
@@ -65,6 +87,8 @@ namespace UltimaXNA.UltimaGUI.WorldGumps
         public override void ActivateByKeyboardReturn(int textID, string text)
         {
             InputState.Text = string.Empty;
+            m_messages.Add(text);
+            messageIndex = m_messages.Count;
             UltimaInteraction.SendChat(text);
         }
 
