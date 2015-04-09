571467a3bfde730c63c0269da8fa96a062e22061
Zane Wagner <zanewagner@gmail.com>
2015-04-09 14:52:39 -0500

    move .nuget to dev folder.

--- plugins/ExamplePlugin/ExamplePluginModule.cs
+++ plugins/ExamplePlugin/ExamplePluginModule.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UltimaXNA.Configuration;
+﻿using UltimaXNA.Configuration;
 using UltimaXNA.Core.Diagnostics.Tracing;
 using UltimaXNA.Core.Patterns.IoC;
 
