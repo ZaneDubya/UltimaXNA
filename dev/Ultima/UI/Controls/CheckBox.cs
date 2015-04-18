c2b94b5939f3f60baa01fd482f8992b1d51679d8
Zane Wagner <zanewagner@gmail.com>
2015-04-18 11:46:39 -0500

    Adding CheckBox base class.
    No code yet.

---
+++ dev/Ultima/UI/Controls/CheckBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UltimaXNA.Core.Graphics;
+
+namespace UltimaXNA.Ultima.UI.Controls
+{
+    class CheckBox : AControl
+    {
+        public CheckBox(AControl owner, int page)
+            : base(owner, page)
+        {
+
+        }
+
+        public override void Draw(SpriteBatchUI spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
