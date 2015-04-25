df153d36cd6ae7f53fdaa81c0cc3c7e18eee8cd6
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-04-25 15:32:05 -0500

    MsgBox on critical error.

---
+++ dev/Core/Diagnostics/Tracing/Listeners/MsgBoxOnCriticalListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltimaXNA.Core.Diagnostics.Tracing.Listeners
+{
+    class MsgBoxOnCriticalListener : AEventListener
+    {
+        public override void OnEventWritten(EventLevel level, string messag)
+        {
+            if (level == EventLevel.Critical)
+                throw new Exception(messag);
+        }
+    }
+}
