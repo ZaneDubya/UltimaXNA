d856b2ee80adb921a8748caff70205c1390a03c2
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-07 19:47:29 -0500

    Broken: moving music to AudioService.

---
+++ dev/Ultima/Audio/UOMusic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltimaXNA.Ultima.Audio
+{
+    class UOMusic
+    {
+        public int Id;
+        public string Name;
+        public bool DoLoop;
+
+        public UOMusic()
+        {
+            Id = -1;
+            Name = "";
+            DoLoop = false;
+        }
+
+        public UOMusic(int id, string name, bool doLoop)
+        {
+            Id = id;
+            Name = name;
+            DoLoop = doLoop;
+        }
+    }
+}
--- dev/Ultima/IO/MusicData.cs
+++ dev/Ultima/IO/MusicData.cs
@@ -189,25 +189,6 @@ namespace UltimaXNA.Ultima.IO
 			InternalStop ();
 		}
 
-        class UOMusic
-        {
-            public int Id;
-            public string Name;
-            public bool DoLoop;
-
-            public UOMusic()
-            {
-                Id = -1;
-                Name = "";
-                DoLoop = false;
-            }
-
-            public UOMusic(int id, string name, bool doLoop)
-            {
-                Id = id;
-                Name = name;
-                DoLoop = doLoop;
-            }
-        }
+        
 	}
 }
