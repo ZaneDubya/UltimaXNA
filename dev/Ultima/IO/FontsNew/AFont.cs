5614bb479d6759176de210efc3b008655086bbe1
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-08 18:54:23 -0500

    Adding more headers!
    Ugh, too many files to add these to! I'm chipping away at them, ten at a
    time.

--- dev/Ultima/IO/FontsNew/AFont.cs
+++ dev/Ultima/IO/FontsNew/AFont.cs
@@ -1,4 +1,14 @@
-﻿#region usings
+﻿/***************************************************************************
+ *   Afont.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
