c898b247179f8ff1a8834cd5fcd153592d4e4224
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-08 20:47:47 -0500

    Stub for spellbook gump.
    Starting work on #82!

---
+++ dev/Ultima/UI/WorldGumps/SpellbookGump.cs
@@ -0,0 +1,36 @@
+﻿/***************************************************************************
+ *   SpellbookGump.cs
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Text;
+using UltimaXNA.Core.Graphics;
+using UltimaXNA.Ultima.Player;
+using UltimaXNA.Ultima.UI;
+using UltimaXNA.Ultima.UI.Controls;
+using UltimaXNA.Core.Input;
+#endregion
+
+namespace UltimaXNA.Ultima.UI.WorldGumps
+{
+    class SpellbookGump : Gump
+    {
+        // Private variables
+
+        // Services
+        private WorldModel m_World;
+
+        public SpellbookGump()
+            : base(0, 0)
+        {
+
+        }
+    }
+}
