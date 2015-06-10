888743b2dff91beabd19269024b939ab70f7aed3
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-10 13:22:07 -0500

    Spellbook object receives spells!

--- dev/Ultima/World/Entities/Items/Containers/SpellBook.cs
+++ dev/Ultima/World/Entities/Items/Containers/SpellBook.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UltimaXNA.Ultima.World.Maps;
+using UltimaXNA.Ultima.Data;
 
 namespace UltimaXNA.Ultima.World.Entities.Items.Containers
 {
@@ -18,15 +19,41 @@ namespace UltimaXNA.Ultima.World.Entities.Items.Containers
             0x2D50 // spellweaving
         };
 
+        /// <summary>
+        ///  Returns true if the parameter ItemID matches a Spellbook Item.
+        /// </summary>
+        /// <param name="itemID">An itemID to be tested</param>
+        /// <returns>True if the itemID is a Spellbook ite, false otherwise.</returns>
         public static bool IsSpellBookItem(ushort itemID)
         {
             return m_SpellBookItemIDs.Contains<ushort>(itemID);
         }
 
+        public SpellBookTypes BookType
+        {
+            get;
+            private set;
+        }
+
+        private bool[] m_SpellData;
+
         public SpellBook(Serial serial, Map map)
             : base(serial, map)
         {
+            BookType = SpellBookTypes.Unknown;
+            m_SpellData = null;
+        }
 
+        public void ReceiveSpellDataFromPacket(SpellbookData data)
+        {
+            BookType = data.BookType;
+
+            if (m_SpellData == null)
+                m_SpellData = new bool[64];
+
+            for (int i = 0; i < 64; i++)
+                if (data.HasSpell(i))
+                    m_SpellData[i] = true;
         }
     }
 }
