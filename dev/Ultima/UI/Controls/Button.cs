dd7227e9a67e86b14c32927d618494e4cee3e43a
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-10 05:38:40 -0500

    Translate gump textures to BGRA5551.
    There's some kind of pre-multiplied alpha problem...

--- dev/Ultima/UI/Controls/Button.cs
+++ dev/Ultima/UI/Controls/Button.cs
@@ -154,10 +154,10 @@ namespace UltimaXNA.Ultima.UI.Controls
 
         protected override bool IsPointWithinControl(int x, int y)
         {
-            Color[] pixelData;
-            pixelData = new Color[1];
-            getTextureFromMouseState().GetData<Color>(0, new Rectangle(x, y, 1, 1), pixelData, 0, 1);
-            if (pixelData[0].A > 0)
+            ushort[] pixelData;
+            pixelData = new ushort[1];
+            getTextureFromMouseState().GetData<ushort>(0, new Rectangle(x, y, 1, 1), pixelData, 0, 1);
+            if (pixelData[0] > 0)
                 return true;
             else
                 return false;
