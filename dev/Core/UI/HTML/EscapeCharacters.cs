3b1d15155981bf04386d61082e07371174133d60
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-20 20:05:41 -0500

    Updating file headers.

--- dev/Core/UI/HTML/EscapeCharacters.cs
+++ dev/Core/UI/HTML/EscapeCharacters.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿/***************************************************************************
+ *   EscapeCharacters.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace UltimaXNA.Core.UI.HTML
 {
