853bc4b76da5a34bae556011a22f61e7d0d2074c
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-25 10:02:36 -0500

    Data added for spell circles 2-4.

--- dev/Ultima/Data/Magery.cs
+++ dev/Ultima/Data/Magery.cs
@@ -22,13 +22,34 @@ namespace UltimaXNA.Ultima.Data
                 new SpellDefinition("Reactive Armor", 0, Reagents.Garlic, Reagents.SpidersSilk, Reagents.SulfurousAsh),
                 new SpellDefinition("Weaken", 0, Reagents.Garlic, Reagents.Nightshade),
                 // second circle
-
+                new SpellDefinition("Agility", 0, Reagents.Bloodmoss, Reagents.MandrakeRoot),
+                new SpellDefinition("Cunning", 0, Reagents.Nightshade, Reagents.MandrakeRoot),
+                new SpellDefinition("Cure", 0, Reagents.Garlic, Reagents.Ginseng),
+                new SpellDefinition("Harm", 0, Reagents.Nightshade, Reagents.SpidersSilk),
+                new SpellDefinition("Magic Trap", 0, Reagents.Garlic, Reagents.SpidersSilk, Reagents.SulfurousAsh),
+                new SpellDefinition("Magic Untrap", 0, Reagents.Bloodmoss, Reagents.SulfurousAsh),
+                new SpellDefinition("Protection", 0, Reagents.Garlic, Reagents.Ginseng, Reagents.SulfurousAsh),
+                new SpellDefinition("Strength", 0, Reagents.MandrakeRoot, Reagents.Nightshade),
                 // third circle
-
+                new SpellDefinition("Bless", 0, Reagents.Garlic, Reagents.MandrakeRoot),
+                new SpellDefinition("Fireball", 0, Reagents.BlackPearl),
+                new SpellDefinition("Magic Lock", 0, Reagents.Bloodmoss, Reagents.Garlic, Reagents.SulfurousAsh),
+                new SpellDefinition("Poison", 0, Reagents.Nightshade),
+                new SpellDefinition("Telekinesis", 0, Reagents.Bloodmoss, Reagents.MandrakeRoot),
+                new SpellDefinition("Teleport", 0, Reagents.Bloodmoss, Reagents.MandrakeRoot),
+                new SpellDefinition("Unlock", 0, Reagents.Bloodmoss, Reagents.SulfurousAsh),
+                new SpellDefinition("Wall of Stone", 0, Reagents.Bloodmoss, Reagents.Garlic),
                 // fourth circle
-
+                new SpellDefinition("Arch Cure", 0, Reagents.Garlic, Reagents.Ginseng, Reagents.MandrakeRoot),
+                new SpellDefinition("Arch Protection", 0, Reagents.Garlic, Reagents.Ginseng, Reagents.MandrakeRoot, Reagents.SulfurousAsh),
+                new SpellDefinition("Curse", 0, Reagents.Garlic, Reagents.Nightshade, Reagents.SulfurousAsh),
+                new SpellDefinition("Fire Field", 0, Reagents.BlackPearl, Reagents.SpidersSilk, Reagents.SulfurousAsh),
+                new SpellDefinition("Greater Heal", 0, Reagents.Garlic, Reagents.Ginseng, Reagents.MandrakeRoot, Reagents.SpidersSilk),
+                new SpellDefinition("Lightning", 0, Reagents.MandrakeRoot, Reagents.SulfurousAsh),
+                new SpellDefinition("Mana Drain", 0, Reagents.BlackPearl, Reagents.MandrakeRoot, Reagents.SpidersSilk),
+                new SpellDefinition("Recall", 0, Reagents.BlackPearl, Reagents.Bloodmoss, Reagents.MandrakeRoot),
                 // fifth circle
-
+                
                 // sixth circle
 
                 // seventh circle
