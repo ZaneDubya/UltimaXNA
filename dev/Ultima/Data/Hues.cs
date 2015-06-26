39b04bb9d265bdbbe7248efa2431c91824e614a7
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-06-25 21:34:09 -0500

    Removing old hues code that depended on System.Drawing.Color.

---
+++ dev/Ultima/Data/Hues.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltimaXNA.Ultima.Data
+{
+    static class Hues
+    {
+        public static int[] SkinTones
+        {
+            get
+            {
+                int max = 7 * 8;
+                int[] hues = new int[max];
+                for (int i = 0; i < max; i++)
+                {
+                    hues[i] = (i < 37) ? i + 1002 : i + 1003;
+                }
+                return hues;
+            }
+        }
+
+        public static int[] HairTones
+        {
+            get
+            {
+                int max = 8 * 6;
+                int[] hues = new int[max];
+                for (int i = 0; i < max; i++)
+                {
+                    hues[i] = i + 1102;
+                }
+                return hues;
+            }
+        }
+    }
+}
