df7a1ec9545363937fb008810a3dafe5b0478d09
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-07-17 07:43:03 -0500

    Move MP3Sharp to Audio namespace, add XNAMP3

--- dev/Ultima/Audio/AudioService.cs
+++ dev/Ultima/Audio/AudioService.cs
@@ -11,26 +11,20 @@
 #region usings
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
+using UltimaXNA.Core.Audio;
 using UltimaXNA.Core.Diagnostics.Tracing;
 using UltimaXNA.Ultima.IO;
-using UltimaXNA.Configuration;
 #endregion
 
 namespace UltimaXNA.Ultima.Audio
 {
     public class AudioService
     {
-        private Dictionary<int, UOSound> m_Sounds = new Dictionary<int, UOSound>();
-        private Dictionary<int, UOMusic> m_Music = new Dictionary<int, UOMusic>();
+        private readonly Dictionary<int, UOSound> m_Sounds = new Dictionary<int, UOSound>();
+        private readonly Dictionary<int, UOMusic> m_Music = new Dictionary<int, UOMusic>();
 
-        private const string c_InternalMusicName = "UltimaXNAMusic";
         private UOMusic m_MusicCurrentlyPlaying = null;
-
-        [DllImport("winmm.dll")]
-        private static extern int mciSendString(string lpCommand, StringBuilder lpReturn, int nReturnLength, IntPtr callBack);
-
+        private XNAMP3 m_MusicCurrentlyPlayingMP3 = null;
 
         public void PlaySound(int soundIndex)
         {
@@ -66,6 +60,7 @@ namespace UltimaXNA.Ultima.Audio
                 if (id < 0) // not a valid id, used to stop music.
                 {
                     StopMusic();
+                    Tracer.Error("Received unknown music id {0}", id);
                     return;
                 }
 
@@ -73,7 +68,7 @@ namespace UltimaXNA.Ultima.Audio
                 {
                     string name;
                     bool loops;
-                    if (UltimaXNA.Ultima.IO.MusicData.TryGetMusicData(id, out name, out loops))
+                    if (MusicData.TryGetMusicData(id, out name, out loops))
                     {
                         m_Music.Add(id, new UOMusic(id, name, loops));
                     }
@@ -85,13 +80,11 @@ namespace UltimaXNA.Ultima.Audio
                 }
 
                 UOMusic toPlay = m_Music[id];
-
                 if (toPlay != m_MusicCurrentlyPlaying)
                 {
                     // stop the current song
                     StopMusic();
 
-                    m_MusicCurrentlyPlaying = null;
                     // if (m_MusicCurrentlyPlaying.Status != SoundState.Loaded)
                     //    m_MusicCurrentlyPlaying.Load(); // this should really be threaded
 
@@ -124,35 +117,9 @@ namespace UltimaXNA.Ultima.Audio
         {
             if (m_MusicCurrentlyPlaying != null)
             {
-                // MediaPlayer.Stop();
-                // Stop playing
-                if (SendMediaPlayerCommand(string.Format("stop {0}", c_InternalMusicName), null, 0, IntPtr.Zero) == 0)
-                {
-                    m_MusicCurrentlyPlaying = null;
-                    // close resource
-                    if (SendMediaPlayerCommand(string.Format("close {0}", c_InternalMusicName), null, 0, IntPtr.Zero) != 0)
-                    {
-                        Tracer.Error("Error closing current mp3 file");
-                    }
-                }
-                else
-                {
-                    Tracer.Error("Error stopping current mp3 file");
-                }
-            }
-        }
-
-        private int SendMediaPlayerCommand(string lpCommand, StringBuilder lpReturn, int nReturnLength, IntPtr callBack)
-        {
-            try
-            {
-                int retval = mciSendString(lpCommand, lpReturn, nReturnLength, callBack);
-                return retval;
-            }
-            catch (Exception ex)
-            {
-                Tracer.Error("Error sending media player command: {0}", ex.Message);
-                return -1;
+                m_MusicCurrentlyPlayingMP3.Stop();
+                m_MusicCurrentlyPlayingMP3.Dispose();
+                m_MusicCurrentlyPlaying = null;
             }
         }
     }
