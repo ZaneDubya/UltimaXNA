06f3dd65df1093913ee5b7d4ac3d8dcac3e39586
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-07-25 10:40:20 -0500

    Base context menu displaying.

--- dev/Ultima/Data/ContextMenuData.cs
+++ dev/Ultima/Data/ContextMenuData.cs
@@ -19,28 +19,38 @@ namespace UltimaXNA.Ultima.Data
 {
     public class ContextMenuData
     {
-        private readonly List<ContextMenuItem> m_entries = new List<ContextMenuItem>();
-        private readonly Serial m_serial;
+        private readonly List<ContextMenuItem> m_Entries = new List<ContextMenuItem>();
+        private readonly Serial m_Serial;
 
         public ContextMenuData(Serial serial)
         {
-            m_serial = serial;
+            m_Serial = serial;
         }
 
         public Serial Serial
         {
-            get { return m_serial; }
+            get { return m_Serial; }
         }
 
-        public ContextMenuItem GetContextEntry(string caption)
+        public int Count
         {
-            return m_entries.FirstOrDefault(i => i.Caption == caption);
+            get { return m_Entries.Count; }
+        }
+
+        public ContextMenuItem this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= m_Entries.Count)
+                    return null;
+                return m_Entries[index];
+            }
         }
 
         // Add a new context menu entry.
         internal void AddItem(int nResponseCode, int nStringID, int nFlags, int nHue)
         {
-            m_entries.Add(new ContextMenuItem(nResponseCode, nStringID, nFlags, nHue));
+            m_Entries.Add(new ContextMenuItem(nResponseCode, nStringID, nFlags, nHue));
         }
     }
 }
--- dev/Ultima/UI/WorldGumps/ContextMenuGump.cs
+++ dev/Ultima/UI/WorldGumps/ContextMenuGump.cs
@@ -17,16 +17,46 @@ using UltimaXNA.Ultima.World.Entities;
 using UltimaXNA.Ultima.World.Entities.Items;
 using UltimaXNA.Ultima.World.Entities.Items.Containers;
 using UltimaXNA.Ultima.Data;
+using UltimaXNA.Core.UI.Fonts;
+using UltimaXNA.Ultima.IO.Fonts;
+using System;
+using System.Text;
 #endregion
 
 namespace UltimaXNA.Ultima.UI.WorldGumps
 {
     class ContextMenuGump : Gump
     {
+        private readonly ContextMenuData m_Data;
+
+        private ResizePic m_Background;
+        private HtmlGumpling m_MenuItems;
+
         public ContextMenuGump(ContextMenuData data)
             : base(0, 0)
         {
+            m_Data = data;
 
+            AFont font = TextUni.GetUniFont(1);
+
+            m_Background = (ResizePic)AddControl(new ResizePic(this, 0, 0, 0x0A3C, 50, font.Height * m_Data.Count + 20));
+
+            StringBuilder htmlContextItems = new StringBuilder();
+            for (int i = 0; i < m_Data.Count; i++)
+            {
+                htmlContextItems.Append(string.Format("<a href='{0}' style='font-decoration:none;'>{1}</a>\n", m_Data[i].ResponseCode, m_Data[i].Caption));
+            }
+            m_MenuItems = (HtmlGumpling)AddControl(new HtmlGumpling(this, 10, 10, 200, font.Height * m_Data.Count, 0, 0, htmlContextItems.ToString()));
+        }
+
+        protected override void OnInitialize()
+        {
+            m_Background.Width = m_MenuItems.Width + 20;
+        }
+
+        public override void Update(double totalMS, double frameMS)
+        {
+            base.Update(totalMS, frameMS);
         }
     }
 }
