40e3bc7051c63e64ee147b75ddd2272884374de5
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-07-31 12:06:17 -0500

    Better variable naming for GumpPic

--- dev/Ultima/UI/Controls/GumpPic.cs
+++ dev/Ultima/UI/Controls/GumpPic.cs
@@ -56,8 +56,8 @@ namespace UltimaXNA.Ultima.UI.Controls
             if (arguements.Length > 4)
             {
                 // has a HUE=XXX arguement (and potentially a CLASS=XXX argument).
-                string huess = arguements[4].Substring(arguements[4].IndexOf('=') + 1);
-                hue = Int32.Parse(huess);
+                string hueArgument = arguements[4].Substring(arguements[4].IndexOf('=') + 1);
+                hue = Int32.Parse(hueArgument);
             }
             buildGumpling(x, y, gumpID, hue);
         }
