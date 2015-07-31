e7ad240b8b600a8629f6b096775ba9a8685fdc4d
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-07-31 10:14:25 -0500

    Adding headers.

--- dev/Ultima/World/Input/WorldInput.cs
+++ dev/Ultima/World/Input/WorldInput.cs
@@ -1,4 +1,14 @@
-﻿#region usings
+﻿/***************************************************************************
+ *   WorldInput.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using UltimaXNA.Configuration;
