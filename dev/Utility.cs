847738a9e1d87fe18b1ff0606444620938944766
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-08-03 12:29:22 -0500

    New capitalization helpers in Utility.

--- dev/Utility.cs
+++ dev/Utility.cs
@@ -487,5 +487,34 @@ namespace UltimaXNA
             if (texture != null)
                 texture.SaveAsPng(new FileStream(path, FileMode.Create), texture.Width, texture.Height);
         }
+
+        public static string CapitalizeFirstCharacter(string str)
+        {
+            if (str == null || str == string.Empty)
+                return string.Empty;
+            if (str.Length == 1)
+                return char.ToUpper(str[0]).ToString();
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+        public static string CapitalizeAllWords(string str)
+        {
+            if (str == null || str == string.Empty)
+                return string.Empty;
+            if (str.Length == 1)
+                return char.ToUpper(str[0]).ToString();
+
+            StringBuilder sb = new StringBuilder();
+            bool capitalizeNext = true;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (capitalizeNext)
+                    sb.Append(char.ToUpper(str[i]));
+                else
+                    sb.Append(str[i]);
+                capitalizeNext = (" .,;!".Contains(str[i]));
+            }
+            return sb.ToString();
+        }
     }
 }
