9f41c2e44485cd8931934f275aa44e22bcd3107d
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-08-24 09:03:22 -0500

    Rename FileIndexes to FileIndexEntry3D and 5D.

---
+++ dev/Ultima/IO/FileIndexEntry3D.cs
@@ -0,0 +1,24 @@
+﻿/***************************************************************************
+ *   FileIndexEntry5D.cs
+ *   Based on code from UltimaSDK: http://ultimasdk.codeplex.com/
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
+using System.Runtime.InteropServices;
+#endregion
+
+namespace UltimaXNA.Ultima.IO
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 0x1)]
+    public struct FileIndexEntry3D
+    {
+        public int lookup;
+        public int length;
+        public int extra;
+    }
+}
