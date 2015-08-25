8b4084e4911a8adf298fa6227988f0271f763c45
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-08-25 10:18:04 -0500

    Breaking out mobilemoveevent class.

---
+++ dev/Ultima/World/Entities/Mobiles/MobileMoveEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltimaXNA.Ultima.World.Entities.Mobiles
+{
+    class MobileMoveEvent
+    {
+        public bool CreatedByPlayerInput = false;
+        public readonly int X, Y, Z, Facing, Fastwalk;
+
+        public MobileMoveEvent(int x, int y, int z, int facing, int fastwalk)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Facing = facing;
+            Fastwalk = fastwalk;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", X, Y, Z);
+        }
+    }
+}
