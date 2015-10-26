26bc57e841f7f9eac9bc8c5fcef8086b0f2eac2d
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-10-26 09:56:19 -0500

    SetScreenSize actually changes *window* size.

--- dev/Core/Resolutions.cs
+++ dev/Core/Resolutions.cs
@@ -1,21 +1,34 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
-using UltimaXNA.Configuration;
+﻿/***************************************************************************
+ *   Resolutions.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
 using Microsoft.Xna.Framework;
-using System.Drawing;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using UltimaXNA.Configuration.Properties;
+#endregion
 
 namespace UltimaXNA.Core
 {
+    /// <summary>
+    /// Contains a list of all valid resolutions, and the code to change the size of the rendered window.
+    /// </summary>
     internal class Resolutions
     {
         public static readonly List<ResolutionProperty> FullScreenResolutionsList;
         public static readonly List<ResolutionProperty> PlayWindowResolutionsList;
 
-        public static void SetScreenSize(GameWindow window)
+        public static void SetWindowSize(GameWindow window)
         {
-            Microsoft.Xna.Framework.Rectangle game;
+            Rectangle game;
             System.Drawing.Rectangle screen;
 
             if (window != null)
@@ -53,7 +66,7 @@ namespace UltimaXNA.Core
             FullScreenResolutionsList = new List<ResolutionProperty>();
             PlayWindowResolutionsList = new List<ResolutionProperty>();
 
-            SetScreenSize(null);
+            SetWindowSize(null);
         }
 
         public static bool IsValidFullScreenResolution(ResolutionProperty resolution)
