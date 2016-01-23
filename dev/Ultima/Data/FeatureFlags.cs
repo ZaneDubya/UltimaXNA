296ac5c89baef7e64283978ea0f83590fe417e00
Zane Wagner <ZaneDubya@users.noreply.github.com>
2016-01-23 16:20:24 -0600

    Adding feature flags.
    @Exadron

--- dev/Ultima/Data/FeatureFlags.cs
+++ dev/Ultima/Data/FeatureFlags.cs
@@ -25,7 +25,7 @@ namespace UltimaXNA.Ultima.Data
         SameraiEmpire = 0x40,
         MondainsLegacy = 0x80,
         Splash8 = 0x100,
-        Splash9 = 0x200,
+        Splash9 = 0x200,            // Ninth Age splash screen, crystal/shadow housing tiles
         TenthAge = 0x400,
         MoreStorage = 0x800,
         CharacterSlot7 = 0x1000,
@@ -33,5 +33,8 @@ namespace UltimaXNA.Ultima.Data
         TrialAccount = 0x4000,
         EleventhAge = 0x8000,
         StygianAbys = 0x10000,
+        HighSeas = 0x20000,         // HS features;
+        GothicHousing = 0x40000,    // Gothic housing tiles;
+        RusticHousing = 0x80000     // Rustic housing tiles
     }
 }
