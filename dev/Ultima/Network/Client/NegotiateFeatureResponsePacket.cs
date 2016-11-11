1ee06acc19a7b24541c5387226e086823652517e
Zane Wagner <ZaneDubya@users.noreply.github.com>
2016-11-10 18:04:57 -0600

    Update header.

--- dev/Ultima/Network/Client/NegotiateFeatureResponsePacket.cs
+++ dev/Ultima/Network/Client/NegotiateFeatureResponsePacket.cs
@@ -1,6 +1,6 @@
 ﻿/***************************************************************************
- *   MoveRequestPacket.cs
- *   Copyright (c) 2009 UltimaXNA Development Team
+ *   NegotiateFeatureResponsePacket.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
