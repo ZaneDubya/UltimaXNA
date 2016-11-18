9ab0592cb4a06d387b1a9d16052f93e32962ecf0
Zane Wagner <ZaneDubya@users.noreply.github.com>
2016-11-18 10:38:19 -0600

    add debug book gump for testing purposes.

---
+++ plugins/ExamplePlugin/DebugShowBookModule.cs
@@ -0,0 +1,27 @@
+﻿using UltimaXNA;
+using UltimaXNA.Core.Patterns;
+using UltimaXNA.Core.UI;
+using UltimaXNA.Ultima;
+using UltimaXNA.Ultima.UI.LoginGumps;
+using UltimaXNA.Ultima.UI.WorldGumps;
+using UltimaXNA.Ultima.World.Entities.Items;
+
+namespace ExamplePlugin {
+    class DebugShowGumpModule : IModule {
+        public string Name => "DebugBookTest";
+
+        public void Load() {
+            LoginGump.AddButton("Debug:Book", OnClickDebugGump);
+        }
+
+        public void Unload() {
+            LoginGump.RemoveButton(OnClickDebugGump);
+        }
+
+        void OnClickDebugGump() {
+            BaseBook book = new BaseBook(Serial.NewDynamicSerial, null);
+            book.ItemID = 0xFEF;
+            ServiceRegistry.GetService<UserInterfaceService>().AddControl(new BookGump(book), 10, 10);
+        }
+    }
+}
